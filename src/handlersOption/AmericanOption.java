36c0b20d7e2931bb00e3c810fc910af2224fba12
Victorius <ricka_elf@mail.ru>
2013-08-14 17:07:48 +0400

    American Call/Put Option

--- src/handlersOption/AmericanOption.java
+++ src/handlersOption/AmericanOption.java
@@ -7,25 +7,36 @@ package handlersOption;
 
 public class AmericanOption extends CommonHandler{    
     public Double[][] S,v;
+    private Double  dt=0.0;
     public AmericanOption(int N){
         S = new Double[N][N];
+        v= new Double[N][N];
         this.optionName="AMERICAN";
     }
     
     @Override
     public Double Call(double S,double X, double r,double rf, double T,double a){
+        fillTreeS(S,a,dt);
+        for(int i=0;i<this.S.length;i++){
+            v[this.S.length-1][i]=call(X,this.S.length-1,i);
+        }
+        for(int i=this.S.length-2;i>=0;i--)
+            for(int j=0;j<=i;j++){
+                double p=getProb(r,rf,dt,Math.exp(a*Math.sqrt(dt)));
+                v[i][j] = Math.max(call(X,i,j), Math.exp(-r*dt)*( p*v[i+1][j]+(1-p)*v[i+1][j+1]));
+            }
         
-        return 0.0;
+        return v[0][0];
     }
     
     @Override
-    public Double Put(double S,double X, double r,double rf, double T,double a){
-        double dt=0.0;
+    public Double Put(double S,double X, double r,double rf, double T,double a){        
+        fillTreeS(S,a,dt);
         for(int i=0;i<this.S.length;i++){
             v[this.S.length-1][i]=put(X,this.S.length-1,i);
         }
-        for(int i=this.S.length-2;i>0;i--)
-            for(int j=0;j<i;j++){
+        for(int i=this.S.length-2;i>=0;i--)
+            for(int j=0;j<=i;j++){
                 double p=getProb(r,rf,dt,Math.exp(a*Math.sqrt(dt)));
                 v[i][j] = Math.max(put(X,i,j), Math.exp(-r*dt)*( p*v[i+1][j]+(1-p)*v[i+1][j+1]));
             }
@@ -34,22 +45,26 @@ public class AmericanOption extends CommonHandler{
     }
     
     public Double put(double X, int i, int j){
-        return 1000*Math.max(X-this.S[j][i],0);        
+        return 1000*Math.max(X-this.S[i][j],0);        
     }
     public Double call(double X, int i, int j){
         return 1000*Math.max(0,this.S[i][j]-X);
     }
     public void fillTreeS(double s0, double a,double dt){
         double u = Math.exp(a*Math.sqrt(dt));
-        for(int i=0;i<S.length;i++)
+        for(int i=0;i<this.S.length;i++)
             for(int j=0;j<i+1;j++){
-                S[i][j]=s0*Math.pow(u, i)*Math.pow(1/u, j-i);                
+                this.S[i][j]=s0*Math.pow(u, i-j)*Math.pow(1/u, j);                
             }
         
     }
     
     public Double getProb(double r, double rf, double dt, double u){
         Double p = (Math.exp((r-rf)*dt) - 1/u)/(u-1/u);
-        return 0.0;
+        return p;
+    }
+    
+    public void setdt(double dt){
+        this.dt=dt;
     }
 }
--- src/helpful_package/test4test.java
+++ src/helpful_package/test4test.java
@@ -6,6 +6,7 @@ package helpful_package;
 
 import cern.jet.random.Normal;
 import cern.jet.random.engine.RandomEngine;
+import handlersOption.AmericanOption;
 import handlersOption.CommonHandler;
 import handlersOption.EuropeanOption;
 import java.io.BufferedReader;
@@ -27,10 +28,12 @@ public class test4test {
 //        ch.Call();
 //        System.out.println(ch.getCurrencyPair());
         Normal n = new Normal(0.0,1.0,RandomEngine.makeDefault());
-        System.out.println(n.cdf(-0.3253));
-        CommonHandler optionHandler = new CommonHandler();
-        double x =optionHandler.Call(1.56, 1.6,0.06, 0.08, 0.5, 0.12);
-        x=optionHandler.Put(1.56, 1.6,0.06, 0.08, 0.5, 0.12);
+//        System.out.println(n.cdf(-0.3253));
+        AmericanOption optionHandler = new AmericanOption(2);
+        Double x =0.0;//optionHandler.Call(1.56, 1.6,0.06, 0.08, 0.5, 0.12);
+        optionHandler.setdt(0.5);
+        x=optionHandler.Call(1.15, 1.15,0.012, 0.022, 0.5, 0.1);
+        
         System.out.println(x);
 //        File f = new File("D:\\temp\\data2Nastya\\historical data\\AUD-USD\\AUDUSD_Candlestick_1_M_ASK_01.03.2008-01.03.2012.csv");
 //        FileReader fr = new FileReader(f);
